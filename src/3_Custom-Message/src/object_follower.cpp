c27943420d10a0c39088f338118fab5633e0ec1c
artisan757 <73025506+artisan757@users.noreply.github.com>
2021-07-06 19:45:45 +0700

    Update object_follower.cpp

--- src/3_Custom-Message/src/object_follower.cpp
+++ src/3_Custom-Message/src/object_follower.cpp
@@ -1,5 +1,7 @@
 // TODO: implementasi object_follower.cpp
 
+//Maaf kak saya tidak bisa soal yang ini
+
 const double VIEW_V_ANGLE = 43.3;  //degree = 0.8028 rad
 const double VIEW_H_ANGLE = 70.42;
 
