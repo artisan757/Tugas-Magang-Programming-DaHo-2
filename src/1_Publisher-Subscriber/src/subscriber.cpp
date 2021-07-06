a8ed45e721732ae2003e1217f49b9b9fe986095b
artisan757 <73025506+artisan757@users.noreply.github.com>
2021-07-06 09:36:26 +0700

    Update subscriber.cpp
    Rizky Anggian Matondang
    16720470

--- src/1_Publisher-Subscriber/src/subscriber.cpp
+++ src/1_Publisher-Subscriber/src/subscriber.cpp
@@ -1,1 +1,42 @@
-// TODO: implementasi subscriber.cpp
+// Nama : Rizky Anggian Matondang
+// NIM  : 16720470
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+using namespace std;
+
+string info;
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  info = msg->data;
+}
+
+void print(){
+  cout << info << endl;
+}
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "subscriber");
+
+
+  ros::NodeHandle n;
+
+
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+  ROS_INFO("Subscriber running . . .");
+
+  ros::Rate rate(60);
+
+  while(ros::ok()){
+    print();
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
+}
