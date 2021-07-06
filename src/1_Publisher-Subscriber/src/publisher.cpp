5d3d067acafa0f1df150a9eb992cd3a7bce4b09a
artisan757 <73025506+artisan757@users.noreply.github.com>
2021-07-06 09:35:48 +0700

    Update publisher.cpp
    Rizky Anggian Matondang
    16720470

--- src/1_Publisher-Subscriber/src/publisher.cpp
+++ src/1_Publisher-Subscriber/src/publisher.cpp
@@ -1,1 +1,37 @@
-// TODO: implementasi publisher.cpp
+// Nama : Rizky Anggian Matondang
+// NIM  : 16720470
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "publisher");
+
+  ros::NodeHandle n;
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+  ROS_INFO("Publisher running . . .");
+
+  ros::Rate loop_rate(60);
+
+  while (ros::ok())
+  {
+
+    std_msgs::String msg;
+
+    msg.data = "Hello, World!";
+
+    chatter_pub.publish(msg);
+
+
+    loop_rate.sleep();
+  }
+
+
+  return 0;
+}
